76936c23dd6526da9b0b878bd61756f6387ba5aa
madhualupana9 <madhualupana7@gmail.com>
2026-03-12 21:50:04 +0530

    commit 8

--- assests/projects/DRR/js/custom.js
+++ assests/projects/DRR/js/custom.js
@@ -36010,7 +36010,7 @@
                     className: "map-container shadow-sm rounded-4 overflow-hidden",
                     children: (0, St.jsx)("iframe", {
                         title: "Google Map",
-                        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.2098000977167!2d78.17473987462832!3d17.449669701009046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbef73289041db%3A0xa0f9f55ccfbc6cf3!2sUrban%20Trilla%20Apartments!5e0!3m2!1sen!2sin!4v1758694083235!5m2!1sen!2sin",
+                        src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.27315183063!2d78.39123831487713!3d17.4363251880503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb9158f201b205%3A0x11bbe7be7792411b!2sKavuri%20Hills%2C%20Madhapur%2C%20Hyderabad%2C%20Telangana%20500033!5e0!3m2!1sen!2sin!4v1625123456789!5m2!1sen!2sin",
                         width: "100%",
                         height: "350",
                         style: { border: 0, minHeight: "100%" },
