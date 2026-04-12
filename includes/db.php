4c369cabc0176e269c70a4421ad2793a2c5b6de4
madhualupana9 <madhualupana7@gmail.com>
2026-04-12 17:36:10 +0530

    commit 3

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = '127.0.0.1';
-$db   = 'paanch';
-$user = 'root';
-$pass = '';
+$db   = 'paanchdevdb';
+$user = 'paanchdev';
+$pass = 'paanreak';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
